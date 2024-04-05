bce44ee7d5d499aa89b22d1f53df25e8022b1128
Mauro Spinelli <m@lanificiodigitale.com>
2024-04-06 00:15:33 +0200

    fix golang

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
+func main() {
+
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/sayHello", greet)
+
+	serverPort := 9090
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", serverPort),
+		Handler: serverMux,
+	}
+	go func() {
+		log.Printf("Starting HTTP Greeter on port %d\n", serverPort)
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP ListenAndServe error: %v", err)
+		}
+		log.Println("HTTP server stopped serving new requests.")
+	}()
+
+	stopCh := make(chan os.Signal, 1)
+	signal.Notify(stopCh, syscall.SIGINT, syscall.SIGTERM)
+	<-stopCh // Wait for shutdown signal
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	log.Println("Shutting down the server...")
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+	log.Println("Shutdown complete.")
 }
 
-func main() {
-	http.HandleFunc("/", helloHandler)
-
-	// Listen on port 8080
-	port := ":8080"
-	fmt.Printf("Server listening on port %s...\n", port)
-	err := http.ListenAndServe(port, nil)
-	if err != nil {
-		panic(err)
+func greet(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "Stranger"
 	}
+	fmt.Fprintf(w, "Hello, %s!\n", name)
 }
